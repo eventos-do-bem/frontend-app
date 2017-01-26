587cc35743ccf2253ebee968b863d1cae1988059
Guilherme Siquinelli <guiseek@gmail.com>
2017-01-26 02:05:15 -0200

    Login e domains production com

--- dist/bundle.js
+++ dist/bundle.js
@@ -25,7 +25,7 @@ function AppConfig($httpProvider, envServiceProvider, $urlRouterProvider, $sceDe
   envServiceProvider.config({
     domains: {
       development: ['127.0.0.1:8000', 'localhost:8000', 'frontend.eventosdobem.com'],
-      production: ['production.eventodobem.com']
+      production: ['production.eventosdobem.com.br', 'eventodobem.com.br', 'eventosdobem.com', 'eventodobem.com', 'eventosdobem.com.br', 'production.eventosdobem.com']
       // anotherStage: ['domain1', 'domain2'], 
       // anotherStage: ['domain1', 'domain2'] 
     },
